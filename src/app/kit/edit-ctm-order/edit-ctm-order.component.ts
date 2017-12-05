00438b9b1122a982f9aba12f47d74bd2f8517d52
Shyam <shyam.rajendran@ameritas.com>
2017-12-05 01:19:30 -0500

    updated form wizard WIP and made color scheme changes

--- src/app/kit/edit-ctm-order/edit-ctm-order.component.ts
+++ src/app/kit/edit-ctm-order/edit-ctm-order.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Order} from '../../order';
+
+declare var $: any;
+declare var jQuery: any;
 
 @Component({
-  selector: 'app-edit-ctm-order',
+  selector: 'cat-page',
   templateUrl: './edit-ctm-order.component.html',
   styleUrls: ['./edit-ctm-order.component.scss']
 })
-export class EditCtmOrderComponent implements OnInit {
 
-  constructor() { }
+export class EditCtmOrderComponent implements OnInit {
+  order = new Order();
+  orders: Order[] = [];
 
   ngOnInit() {
-  }
 
+    $(function () {
+
+      $("#example-icons").steps({
+        headerTag:  'h3',
+        bodyTag:  'section',
+        transitionEffect:  'slideLeft',
+        autoFocus: true
+      });
+    });
+
+  }
 }
+
