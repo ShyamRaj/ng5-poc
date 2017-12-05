37378666f1ee4e185dd631059a54aad73f02fdf8
Shyam <shyam.rajendran@ameritas.com>
2017-12-05 10:46:24 -0500

    fixed the data table display issue and moved the form wizard to study management

--- src/app/study-management/study-management.component.ts
+++ src/app/study-management/study-management.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import {Order} from '../order';
+
+declare var $: any;
+declare var jQuery: any;
 
 @Component({
-  selector: 'app-study-management',
+  selector: 'cat-page',
   templateUrl: './study-management.component.html',
   styleUrls: ['./study-management.component.scss']
 })
 export class StudyManagementComponent implements OnInit {
-
-  constructor() { }
+  order = new Order();
+  orders: Order[] = [];
 
   ngOnInit() {
-  }
 
+    $(function () {
+
+      $("#example-icons").steps({
+        headerTag:  'h3',
+        bodyTag:  'section',
+        transitionEffect:  'slideLeft',
+        autoFocus: true
+      });
+    });
+
+  }
 }
